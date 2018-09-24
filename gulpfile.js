3187989d8231b1b0e394cb4dca68807204d9898c
nicholas <musicin3d@gmail.com>
2018-09-23 23:21:57 -0500

    config, customization, bug fix, first build tested
    (cherry picked from commit 4fc5d72 in another project)
    (modified from original commit)

--- gulpfile.js
+++ gulpfile.js
@@ -16,31 +16,31 @@ gulp.task('generate', shell.task('eleventy'));
 
 
 
-/*
-  compile the assets to the correct destination
-*/
-gulp.task('assets', gulp.parallel(
-  'images',
-  'styles',
-  'scripts'
-));
+// /*
+//   compile the assets to the correct destination
+// */
+// gulp.task('assets', gulp.parallel(
+//   'images',
+//   'styles',
+//   'scripts'
+// ));
 
 
-/*
-  Let's build this sucker, without getting data from online sources
-*/
-gulp.task('build:local', gulp.series(
-  'clean-build',
-  'generate',
-  'assets'
-));
-
-
-/*
-  Let's gwt the data we need and then build this sucker.
-*/
-gulp.task('build', gulp.series(
-  // 'get:data',
-  'generate',
-  'assets'
-));
+// /*
+//   Let's build this sucker, without getting data from online sources
+// */
+// gulp.task('build:local', gulp.series(
+//   'clean-build',
+//   'generate',
+//   'assets'
+// ));
+//
+//
+// /*
+//   Let's get the data we need and then build this sucker.
+// */
+// gulp.task('build', gulp.series(
+//   // 'get:data',
+//   'generate',
+//   'assets'
+// ));
